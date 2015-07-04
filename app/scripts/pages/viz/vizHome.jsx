28845d02a793b04c2ffaa3b21a7d2886ded1a38f
Tim York <tsyork@gmail.com>
2015-07-03 18:25:13 -0700

    Added Dockerfile and fixed Home link to nav bar version of landing page

--- app/scripts/pages/viz/vizHome.jsx
+++ app/scripts/pages/viz/vizHome.jsx
@@ -29,7 +29,7 @@ var HomeViz = React.createClass({
 
     componentDidMount: function(){
         var vizDiv = document.getElementById('viz_container');
-        var vizURL = "https://10ay.online.tableau.com/#/site/gcvdealsdata/views/Subscription_Beta/Landing_Page";
+        var vizURL = "https://10ay.online.tableau.com/#/site/gcvdealsdata/views/Subscription_Beta/Landing_page";
         var options = {
             //width: '1200px',
             //height: '540px',
