23408959379ddf369b46b361f0a6cb60e2a223bd
Tim York <tsyork@gmail.com>
2015-07-04 08:00:26 -0700

    Mods to Dockerfile; add start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+cd /tmp
+
+# try to remove the repo if it already exists
+rm -rf https://github.com/tsyork/gcvdata_v5; true
+
+git clone https://github.com/tsyork/gcvdata_v5
+
+cd gcvdata_v5
+
+npm install
+
+node .
